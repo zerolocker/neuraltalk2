68411d21c870c7070772cc8fc2d7644ffee51fb9
zerolocker <zero-lzd@163.com>
2016-04-22 14:46:57 -0500

    finish reranker (now it can run and output nearest neighbour)

---
+++ cmd.sh
@@ -0,0 +1,1 @@
+th eval.lua -model model_id1-501-1448236541.t7 -image_folder myimg/  -beam_size 3
--- misc/Reranker.lua
+++ misc/Reranker.lua
@@ -2,10 +2,11 @@ require 'torch'
 require 'nn'
 require 'image'
 require 'loadcaffe'
+w2v = require 'word2vec.torch/w2vutils'
 
 local Reranker, parent = torch.class('nn.Reranker', 'nn.Module')
 
-function Reranker:__init(params, proto_file, model_file, word2vec, vocab, vocab_size)
+function Reranker:__init(params, proto_file, model_file, word2vec_karpathy, vocab, vocab_size)
   parent.__init(self)
 
   -- load VGG
@@ -42,55 +43,29 @@ function Reranker:__init(params, proto_file, model_file, word2vec, vocab, vocab_
     end
   end
 
-  -- set up vocabuary and inverse vocabulary
+  -- set up the vocabuary of dataset(not ImageNet) and inverse vocabulary
   self.vocab = vocab
   self.inv_vocab = {}
   for i = 1, vocab_size do
     self.inv_vocab[vocab[tostring(i)]] = i
   end
 
-  -- TODO:
-  -- currently the word2vec function is the learned embedding of Karpathy's alignment model
-  -- may use Google's word2vec trained on other corpus(billions of words)
-  -- but which one is better?
-  self.word2vec = word2vec
-
-  -- load ILSVRC12 synset_word
+  -- load ILSVRC12 synset_word and transform into vec
   self.synset_words = {}
+  self.synset_vecs = torch.Tensor(1000,w2v.M:size(2)):fill(0)
+  self.unk = {}
+  i = 1
   for line in io.lines'models/synset_words.txt' do
-    line = line:sub(11)
-    phrases = {}
-    for phrase in string.gmatch(line, '([^,]+)') do
-      table.insert(phrases, phrase)
+    local words = {}
+    for word in string.gmatch(line, '([^|]+)') do
+      table.insert(words, tostring(word:gsub('[ -]','_'))) -- replace space with _ (in accordance to word2vec)
     end
-    table.insert(self.synset_words, phrases)
+    table.insert(self.synset_words, words)
+    self.synset_vecs[i], self.unk[i] = self:get_words_vec(words)
+    i = i + 1
   end
-  --print(self.synset_words)
-
-  -- TODO: 
-  -- map ILSVRC12 synset_word to vector representation
-  -- not a perfect method
-  self.synset_vecs = {}
-  local l = 0
-  for line in io.lines'models/synset_words.txt' do
-    line = line:sub(11)
-    words = {}
-    for phrase in string.gmatch(line, '([^,]+)') do
-      for word in string.gmatch(phrase, '([^ ]+)') do
-        if (self.inv_vocab[word] == nil) then
---          print(word .. ' is out of vocabulary')
-        else
-          table.insert(words, self.inv_vocab[word])
-        end
-      end
-    end
-
-    if (#words > 0) then
-      local vecs = self.word2vec:forward(torch.Tensor(words))
-      self.synset_vecs[l] = vecs:float() -- entries will have same size if using cudaTensor, why?
-    end
-    l = l + 1
-  end
+  true_cnt=0;for i,b in ipairs(self.unk) do if b==true then true_cnt=true_cnt+1 end end
+  -- print('OOV word cnt in ImageNet 1000 classes:', truecnt)
 end
 
 
@@ -98,21 +73,22 @@ function Reranker:rank(beams, images)
   local batchSize = #beams
   local seq = torch.LongTensor(beams[1][1].seq:size(1), batchSize)
 
-  -- TODO: implement reranking
+
   for k = 1, batchSize do
     local pred = self.cnn:forward(images[k])
     for i = 1, #beams[k] do
       local logP2 = 0
       local sent = beams[k][i].seq     
-      local sentVec = self.word2vec:forward(sent)
       local n = 0
       local sent_str = ''
       for j = 1, sent:size(1) do
-        local label = 1 --nearestNeighbor(sentVec[j], self.synset_vecs)
-        logP2 = logP2 + torch.log(pred[label])
+        local word_idx = self:NN_in_ImgNet(self.vocab[tostring(sent[j])]) -- index in the 1000 ImageNet class nouns
+        local near_word_in_ImgNet = (word_idx > 0) and (table.concat(self.synset_words[word_idx])) or ('')
+        logP2 = logP2 + torch.log(pred[word_idx])
         
         n = n + 1
- 	sent_str = sent_str .. ' ' .. (self.vocab[tostring(sent[j])]==nil and 'nil'..sent[j] or self.vocab[tostring(sent[j])])
+        sent_str = sent_str .. ' ' .. (self.vocab[tostring(sent[j])]==nil and 'nil'..sent[j] or  (self.vocab[tostring(sent[j])] .. '('..near_word_in_ImgNet..')' ) )
+        
       end
       print(sent_str,'logProb: ' .. beams[k][i].p)
       alpha = 0.5
@@ -123,4 +99,47 @@ function Reranker:rank(beams, images)
   end
 
   return seq
+end
+
+  -- return the average the vector of a list of words
+  --  If a word is not in the dict(<UNK>), skip it. If all words are <UNK>, return the vector of <UNK>.
+function Reranker:get_words_vec(words)
+  local vec =  torch.Tensor(w2v.M:size(2)):fill(0)
+  local cnt = 0
+  for i, word in ipairs(words) do 
+    local ind = w2v.w2vvocab[word]
+    if ind == nil then -- if not found, try if the last word of this word phrase exists in word2vec vocabulary
+      last_word_idx = string.find(word, '%a+$')
+      if last_word_idx then
+        ind = w2v.w2vvocab[string.sub(word,last_word_idx)]
+      else
+        -- still not found, skip
+      end
+    end
+    if ind ~= nil then
+      cnt = cnt + 1
+      vec = vec + w2v.M[ind]
+    end
+  end
+  if cnt == 0 then
+    return w2v.UNK, true
+  else
+    return vec / cnt, false
+  end
+end
+
+function Reranker:NN_in_ImgNet(word_str) -- nearest neighbour in ImageNet 1000 class nouns, returns -1 if the minimum distance is greater than a threshold (too far away)
+  -- similarity mesure: -dotProduct 
+  local THRESHOLD = 0.5
+  local vec = self:get_words_vec({word_str}) -- (can use cache to speed it up) (possibly be the vector of <UNK>)
+  if torch.norm(vec-w2v.UNK) < 1e-5 then
+    return -1
+  end
+  local dotprod = torch.mv(self.synset_vecs ,vec)
+  dotprod , idx = torch.sort(dotprod,1,true)
+  if dotprod[1] < THRESHOLD then
+    return -1
+  else
+    return idx[1]
+  end
 end
